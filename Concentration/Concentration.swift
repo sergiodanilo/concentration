67b55139839b5d15e730af9bb508ea4fe5ad6504
Sérgio Danilo <sergioholanda@MacBook-Pro-de-Sergio.local>
2019-03-23 10:27:54 -0300

    Fix variable

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -12,23 +12,23 @@ class Concentration {
     
     var cards = [Card]()
     
-    var indexOfOndeAndOnlyFaceUpCard: Int?
+    var indexOfOneAndOnlyFaceUpCard: Int?
     
     func chooseCard(at index: Int) {
         if !cards[index].isMatched {
-            if let matchedIndex = indexOfOndeAndOnlyFaceUpCard, matchedIndex != index {
+            if let matchedIndex = indexOfOneAndOnlyFaceUpCard, matchedIndex != index {
                 if cards[matchedIndex].identifier == cards[index].identifier {
                     cards[matchedIndex].isMatched = true
                     cards[index].isMatched = true
                 }
                 cards[index].isFaceUp = true
-                indexOfOndeAndOnlyFaceUpCard = nil
+                indexOfOneAndOnlyFaceUpCard = nil
             } else {
                 for flipDownIndex in cards.indices {
                     cards[flipDownIndex].isFaceUp = false
                 }
                 cards[index].isFaceUp = true
-                indexOfOndeAndOnlyFaceUpCard = index
+                indexOfOneAndOnlyFaceUpCard = index
             }
         }
     }
